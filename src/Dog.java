49665faa1e4e93c6343509596c78ebab52c4ecce
Haile Asnake <hasnake@sbgtv.com>
2018-04-19 13:28:34 -0700

    Adding Dog and Movie classes

---
+++ src/Dog.java
@@ -0,0 +1,11 @@
+/**
+ * Created by hasnake on 4/19/18.
+ */
+public class Dog {
+    int size;
+    String breed;
+    String name;
+    void bark(){
+        System.out.println("Ruff!,Ruff!");
+    }
+}
---
+++ src/Movie.java
@@ -0,0 +1,11 @@
+/**
+ * Created by hasnake on 4/19/18.
+ */
+public class Movie {
+    String title;
+    String genre;
+    int rating;
+    void playIt(){
+        System.out.println("Playing the movie");
+    }
+}
