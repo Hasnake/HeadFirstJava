11617f9dc39005a5a9463774e92f82f9147aafcc
Haile Asnake <hasnake@sbgtv.com>
2018-04-26 11:50:51 -0700

    Adding Date exercise

---
+++ src/Dayswithif.java
@@ -0,0 +1,39 @@
+/**
+ * Created by hasnake on 4/26/18.
+ */
+public class Dayswithif {
+    public static void main(String[] args) {
+        int day = 0;
+        if (day == 1) {
+            System.out.println("It's Monday, the 1st day of the week");
+            day= day+1;
+        }
+        if (day == 2) {
+                System.out.println("It's Tuesday, the 2nd day of the week");
+                day=day+1;
+        }
+            if (day == 3) {
+                System.out.println("It's Wednesday, the 3rd day of the week");
+                day=day+1;
+            }
+            if (day == 4) {
+                System.out.println("It's Thursday, the 4th day of the week");
+                day=day+1;
+            }
+            if (day == 5) {
+                System.out.println("It's Friday, the 5th day of the week");
+                day=day+1;
+            }
+            if (day == 6) {
+                System.out.println("It's Saturday, the 6th day of the week");
+                day = day + 1;
+            }
+            if (day == 7) {
+                System.out.println("It's Sunday, the 7th day of the week");
+            }
+
+
+        }
+    }
+
+
