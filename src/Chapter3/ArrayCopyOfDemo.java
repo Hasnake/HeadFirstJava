cccb748681a3e00a76eb0687f355d70a6e7274b8
Haile Asnake <hasnake@sbgtv.com>
2018-05-08 16:47:38 -0700

    Copying array data

---
+++ src/Chapter3/ArrayCopyOfDemo.java
@@ -0,0 +1,16 @@
+package Chapter3;
+
+/**
+ * Created by hasnake on 5/8/18.
+ */
+public class ArrayCopyOfDemo {
+    public static void main(String[] args) {
+
+        char[] copyFrom = {'d', 'e', 'c', 'a', 'f', 'f', 'e',
+                'i', 'n', 'a', 't', 'e', 'd'};
+
+        char[] copyTo = java.util.Arrays.copyOfRange(copyFrom, 2, 9);
+
+        System.out.println(new String(copyTo));
+    }
+}
