950580ed0b54b81f49aedb11ceafc61ec0a1d340
Haile Asnake <hasnake@sbgtv.com>
2018-05-31 12:54:44 -0700

    Adding all

---
+++ src/Chapter5/counterLoop.java
@@ -0,0 +1,9 @@
+package Chapter5;
+
+public class counterLoop {
+    public static void main(String[] args){
+        for (int counter=1;counter<21;counter=counter+3){
+            System.out.println(counter);
+        }
+    }
+}
