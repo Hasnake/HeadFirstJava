9804b70b77eb46701e1fd6223900788901b95e69
Haile Asnake <hasnake@sbgtv.com>
2018-05-31 10:57:59 -0700

    Adding all

--- src/Chapter3/Vehicle.java
+++ src/Chapter3/Vehicle.java
@@ -14,29 +14,43 @@ import java.util.Scanner;
 
 public class Vehicle {
 
-    int numOfWheels=10;
+    int numOfWheels;
     double fuelTankCapacity=24.6;
     float maxSpeed=78.888f;
     double numOfGallonGasTank=8;
-    String modelName="Toyota";
+    String modelName="v";
     String color="Blue";
     int numOfSeats=8;
     String modelSubClass="Camry";
     Boolean hasNavigationGPS=true;
 
 
-    public static void main(String[] args){
-        Vehicle toyota =new Vehicle();
+//Getter and Setter
 
-        System.out.println("numOfWheels:" + toyota.numOfWheels);
-        System.out.println("fuelTankCapacity:" + toyota.fuelTankCapacity);
-        System.out.println("maxSpeed:" + toyota.maxSpeed);
-        System.out.println("numOfGallonGasTank:" + toyota.numOfGallonGasTank);
-        System.out.println("modelName:" + toyota.modelName);
-        System.out.println("color:" + toyota.color);
-        System.out.println("numOfSeats:" + toyota.numOfSeats);
-        System.out.println("modelSubClass:" + toyota.modelSubClass);
-        System.out.println("hasNavigationGPS:" + toyota.hasNavigationGPS);
+    public int getNumOfWheels() {
+        return this.numOfWheels;
+
+    }
+
+    public void setNumOfWheels(int numOfWheels) {
+        this.numOfWheels = 10;
+    }
+
+
+    //Vehicle
+
+    public static void main(String[] args){
+        Vehicle v =new Vehicle();
+
+       //System.out.println("numOfWheels:" + v.getNumOfWheels);
+        System.out.println("fuelTankCapacity:" + v.fuelTankCapacity);
+        System.out.println("maxSpeed:" + v.maxSpeed);
+        System.out.println("numOfGallonGasTank:" + v.numOfGallonGasTank);
+        System.out.println("modelName:" + v.modelName);
+        System.out.println("color:" + v.color);
+        System.out.println("numOfSeats:" + v.numOfSeats);
+        System.out.println("modelSubClass:" + v.modelSubClass);
+        System.out.println("hasNavigationGPS:" + v.hasNavigationGPS);
         System.out.print("Input any model you like: ");
         Scanner input = new Scanner (System.in);
         String yourVehicleOfChoice = input.next();
