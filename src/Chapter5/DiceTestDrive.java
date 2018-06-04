92b066b81f235f00269f294e62b14321c6ba0bb4
Haile Asnake <hasnake@sbgtv.com>
2018-06-04 10:09:54 -0700

    Adding all

---
+++ src/Chapter5/DiceTestDrive.java
@@ -0,0 +1,15 @@
+package Chapter5;
+
+public class DiceTestDrive {
+    public static void main(String[] args) {
+        int random = (int) (Math.random() * 6 + 1);
+        System.out.println(random);
+        int[] dice1 = new int[]{1, 2, 3, 4, 5, 6};
+        int[] dice2 = new int[]{1, 2, 3, 4, 5, 6};
+        int[] dice3 = new int[]{1, 2, 3, 4, 5, 6};
+
+        for (int i : dice1) {
+//            System.out.println(i);
+        }
+    }
+}
--- src/Chapter5/counterLoop.java
+++ src/Chapter5/counterLoop.java
@@ -5,5 +5,13 @@ public class counterLoop {
         for (int counter=1;counter<21;counter=counter+3){
             System.out.println(counter);
         }
+        System.out.println("out of scope");
+
+        String[] fruits = new String[] { "Orange", "Apple", "Pear", "Strawberry" };
+
+        for (String i : fruits) {
+            System.out.println(i);
+            // fruit is an element of the `fruits` array.
+        }
     }
 }
